3e3f59dae7cfd92b2a17f2beb8bb51e8c875b574
qq_60134168 <yujiangtao11@h-partners.com>
2026-02-24 11:34:11 +0800

    [BugFix] A3编译告警清理
    Co-authored-by: yujiangtao<yujiangtao11@h-partners.com>
    
    
    
    # message auto-generated for no-merge-commit merge:
    !336 merge warning into master
    
    [BugFix] A3编译告警清理
    
    Created-by: qq_60134168
    Commit-by: yujiangtao
    Merged-by: cann-robot
    Description: ## 描述
    <!--在这里详细描述你的改动，包括改动的原因和所采取的方法。-->
    A3编译告警清理
    A3 TQUANT.hpp添加至include/pto/common/pto_instr_impl.hpp
    ## 关联的Issue
    <!-- 如果这个PR是为了解决特定的Issue，请在这里提供Issue链接。-->
    <!-- 如果这个PR是为了解决特定的问题单，请在这里描述问题单单号。-->
    
    ## 测试
    <!--描述进行了哪些测试来验证你的改动。包括但不限于二级冒烟、算子泛化等。-->
    
    ## 文档更新
    <!--如果这个PR包含文档的更新，请在这里指出。例如：更新了README.md文件。-->
    
    ## 类型标签
    <!--  [x] 表示选中 -->
    - [ ] Bug修复
    - [ ] 新特性
    - [ ] 性能优化
    - [ ] 文档更新
    - [ ] 其他，请描述：
    
    
    
    
    See merge request: cann/pto-isa!336

--- include/pto/common/pto_instr_impl.hpp
+++ include/pto/common/pto_instr_impl.hpp
@@ -101,6 +101,7 @@ See LICENSE in the root of the software repository for the full text of the Lice
 #include "pto/npu/a2a3/TColExpandMul.hpp"
 #include "pto/npu/a2a3/TColExpandDiv.hpp"
 #include "pto/npu/a2a3/TColExpandExpdif.hpp"
+#include "pto/npu/a2a3/TQuant.hpp"
 #endif
 
 #ifdef PTO_NPU_ARCH_A5
